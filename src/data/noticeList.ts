609e84ec1b13ec814ecf84b383c6e8ae519e5967
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-01-09 17:36:44 +0900

    🌱

---
+++ src/data/noticeList.ts
@@ -0,0 +1,28 @@
+import type { Notice } from "src/constants/types";
+
+export const noticeList: Notice[] = [
+  {
+    id: "1",
+    title: "学生とマッチングしました",
+    body: "",
+    isRead: false,
+  },
+  {
+    id: "2",
+    title: "学生から新着メッセージがあります",
+    body: "",
+    isRead: false,
+  },
+  {
+    id: "3",
+    title: "学生とマッチングしました",
+    body: "",
+    isRead: true,
+  },
+  {
+    id: "4",
+    title: "学生から新着メッセージがあります",
+    body: "",
+    isRead: true,
+  },
+];
