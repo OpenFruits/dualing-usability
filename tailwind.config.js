07d860c5a76c80bdf43935c774acae756436e996
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-01-12 18:26:02 +0900

    🔧

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  darkMode: false, // 'media' or 'class'
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        theme: "#9DD6E5",
+        "theme-light": "#CEEBF2",
+        "theme-dark": "#424242",
+      },
+      backgroundImage: {
+        "first-view": "url('/firstview_bg.png')",
+        persons: "url('/persons.png')",
+      },
+    },
+    boxShadow: {
+      md: "0 4px 6px -1px rgba(0, 0, 0, 0.4), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+    },
+  },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/line-clamp")],
 };
