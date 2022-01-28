94e12553d963ef354a810885912762154b83908e
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-01-28 15:43:36 +0900

    ♻️

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/router";
 import type { VFC } from "react";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { Button } from "src/component/Button";
 import type { Student } from "src/constants/types";
 
 type Props = {
@@ -13,6 +14,7 @@ type Props = {
 export const Card: VFC<Props> = (props) => {
   const router = useRouter();
   const [isBookmark, setIsBookmark] = useState(false);
+  const relation = props.student.relation;
 
   // 保存or保存の削除
   const bookmark = async () => {
@@ -36,15 +38,15 @@ export const Card: VFC<Props> = (props) => {
         style={{ backgroundImage: `url(/student.jpg)` }}
         onClick={() => router.push(`/student/${props.student.uid}`)}
       >
-        {props.student.relation === "scout" && (
+        {relation === "scout" && (
           <div className="absolute top-0 right-0 p-2 m-2 text-sm font-bold text-white bg-emerald-500 rounded-full">
             スカウト中
           </div>
         )}
-        {props.student.relation === "match" && (
+        {relation === "match" && (
           <div className="absolute top-0 right-0 p-2 m-2 text-sm font-bold bg-yellow-300 rounded-full">マッチング</div>
         )}
-        {props.student.relation === "match" && (
+        {relation === "match" && (
           <div className="absolute top-0 left-0 p-2 m-2 text-sm font-bold bg-gray-200 rounded">
             {`${props.student.firstName} ${props.student.lastName}`}
           </div>
@@ -69,21 +71,9 @@ export const Card: VFC<Props> = (props) => {
       <div className="flex justify-start p-2">
         <Link href={`/student/${props.student.uid}`}>
           <a className="mr-2">
-            {props.student.relation === "no" && (
-              <button className="p-2 m-auto text-xs font-bold tracking-wider text-center text-white bg-blue-500 hover:bg-blue-400 rounded focus:outline-none cursor-pointer lg:mx-0">
-                詳細・スカウト画面へ
-              </button>
-            )}
-            {props.student.relation === "scout" && (
-              <button className="p-2 m-auto text-xs font-bold tracking-wider text-center text-white bg-blue-500 hover:bg-blue-400 rounded focus:outline-none cursor-pointer lg:mx-0">
-                詳細画面へ
-              </button>
-            )}
-            {props.student.relation === "match" && (
-              <button className="p-2 m-auto text-xs font-bold tracking-wider text-center text-white bg-blue-500 hover:bg-blue-400 rounded focus:outline-none cursor-pointer lg:mx-0">
-                詳細・チャット画面へ
-              </button>
-            )}
+            {relation === "no" && <Button className="text-xs">詳細・スカウト画面へ</Button>}
+            {relation === "scout" && <Button className="text-xs">詳細画面へ</Button>}
+            {relation === "match" && <Button className="text-xs">詳細・チャット画面へ</Button>}
           </a>
         </Link>
         <button
@@ -91,7 +81,7 @@ export const Card: VFC<Props> = (props) => {
           className="flex pr-2 m-auto text-xs font-bold tracking-wider text-center text-white bg-blue-500 hover:bg-blue-400 rounded focus:outline-none cursor-pointer lg:mx-0"
         >
           <BookmarkIcon className="m-1 w-6 h-6" />
-          {isBookmark ? <p className="py-2">保存済み</p> : <p className="py-2">保存して後で見る</p>}
+          <p className="py-2">{isBookmark ? "保存済み" : "保存して後で見る"}</p>
         </button>
       </div>
     </div>
