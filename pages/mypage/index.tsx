f460654f84a79f4293b40a05de9e85d29146c60c
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-01-29 06:35:29 +0900

    🚸 improve ux

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,5 +1,6 @@
 import Vimeo from "@u-wave/react-vimeo";
 import type { NextPage } from "next";
+import { useState } from "react";
 import { Footer } from "src/component/Footer";
 import { Header } from "src/component/Header";
 import { Inform } from "src/component/student/Inform";
@@ -9,6 +10,9 @@ import { ScoutList } from "src/component/student/ScoutList";
 import { studentList } from "src/constants/data/studentList";
 
 const Mypage: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const loaded = () => setIsLoading(false);
+
   return (
     <div className="box-border relative pb-[140px] min-h-screen">
       <Header pageTitle="マイページ" />
@@ -16,7 +20,17 @@ const Mypage: NextPage = () => {
         <Inform />
         <Profile />
         <div>
-          <Vimeo video={studentList[0].vimeoUrl} responsive className="m-2" />
+          {isLoading && (
+            <div className="aspect-video grid place-items-center m-2 text-2xl text-gray-800 bg-gray-400">
+              Loading...
+            </div>
+          )}
+          <Vimeo
+            video={studentList[0].vimeoUrl}
+            responsive
+            onLoaded={loaded}
+            className={isLoading ? "hidden" : "m-2"}
+          />
           <ScoutList />
           <MatchingList />
         </div>
