0422382bf11d3351e7a1239bfe113a70d61e6242
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-01-30 06:26:09 +0900

    🔧

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,6 @@ module.exports = {
         "theme-light": "#CEEBF2",
         "theme-dark": "#424242",
       },
-      backgroundImage: {
-        "first-view": "url('/firstview_bg.png')",
-        persons: "url('/persons.png')",
-      },
       boxShadow: {
         md: "0 4px 6px -1px rgba(0, 0, 0, 0.4), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
       },
